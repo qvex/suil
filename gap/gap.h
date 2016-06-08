3a19cefe975021e1f91978ac78b91f014775f603
Carter Mbotho <lastcarter@gmail.com>
2016-06-08 14:14:17 -0400

    GAP-1: Cleaning up some file and removing crow as a submodule

---
+++ gap/gap.h
@@ -0,0 +1,12 @@
+//
+// Created by dc on 6/8/16.
+//
+
+#ifndef GAR_GAP_H
+#define GAR_GAP_H
+
+#include "log.h"
+#include "resource_router.h"
+#include "app.h"
+
+#endif //GAR_GAP_H
--- suil/main.cpp
+++ suil/main.cpp
@@ -2,28 +2,9 @@
 // Created by dc on 6/6/16.
 //
 
-#include <regex>
-#include <iostream>
-#include <assert.h>
-#include "trie.h"
-#include <unordered_map>
-
-
-#include "route_params_parser.h"
 
 int main(int argc, char* argv[])
 {
-
-
-    gap::RouteParamsParser parser(gap::RouteParamsParser::compile(
-            "api/users/{id:int}/{name:string}/{id2:float}/{name2:string}"));
-
-    bool status;
-    gap::RouteParams params = parser.build("/api/users/400/carter/600/mpho", status);
-    if (status)
-    printf("id=%d name=%s, id2=%f name2=%s\n",
-            params["id"].i(), params["name"].s().c_str(), params["id2"].f(), params["name2"].s().c_str());
-
     return 0;
 }
 
